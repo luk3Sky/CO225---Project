d1b8d9b01da8d29acb511de51c69a8a4a914925b
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-11-13 21:37:40 +0530

    initial

---
+++ Mandrelbot.java
@@ -0,0 +1,16 @@
+/*
+*
+*/
+
+import java.util.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Fractal extends JFrame{
+    static final int WIDTH=800;
+    static final int HEIGHT=800;
+
+    
+}

611ecc28298395494f82b4d3fa38896620f764f6
luk3Sky <ganindudananja@gmail.com>
2018-11-14 11:25:41 +0530

    Started julia

--- Fractal zoom/FractalExplorer.java
+++ Fractal zoom/FractalExplorer.java
@@ -5,142 +5,247 @@
  * E/15/142 - Jayalath A.H.G.D. 
  */
 
-import java.awt.*; /* java abstract window toolkit */
-import java.awt.event.*; 
+/*
+*
+*/
+
+import java.util.*;
+import java.awt.*;
 import java.awt.image.*;
+import java.awt.event.*;
 import javax.swing.*;
-import java.awt.geom.Line2D;
-import java.util.Random;
 
+public class FractalExplorer extends JFrame{    //inherit JFrame
+    /*
+    global variables for frame size
+    */
+    static final int WIDTH=800;
+    static final int HEIGHT=800;
 
-public class FractalExplorer extends JFrame{
-    
-    // Private instantaneous varaibles.
-    private final int WIDTH = 800;
-    private final int HEIGHT = 800;    
+    //may change 
+    static final int MAX_N=1000;    //maximum no. of iterations
 
-	Canvas canvas;
-	BufferedImage fractalImage;
-	
-	// static final int MAX_ITER = 200;
+    //panel
+    Panel panel;
+    BufferedImage fractalImage;
 
-	public FractalExplorer() {
-		setInitialGUIProperties();
-		addCanvas();
-		// canvas.addKeyStrokeEvents();
-		// updateFractal();
+    /*
+    constructor
+    */
+    public FractalExplorer(){
+        setGUIprop();
+        addPanel();
+        updateFractal();
 		this.setVisible(true);
-	}
-
-	private void addCanvas() {
-
-		canvas = new Canvas();
-		fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-		canvas.setVisible(true);
-		this.add(canvas, BorderLayout.CENTER);
-
-	} // addCanvas
-
-    private void setInitialGUIProperties() {
-			
-        this.setTitle("Fractal Explorer");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(WIDTH, HEIGHT);
-        this.setResizable(false);
-        this.setLocationRelativeTo(null);
-        
-    } // setInitialGUIProperties
-
-    @Override public Dimension getPreferredSize() {
-        return new Dimension(WIDTH, HEIGHT);
-    } // getPreferredSize
-
-    public static void main(String[] args) {
-        new FractalExplorer();
     }
 
-    private class Canvas extends JPanel {
+
+    //may change---------------------------------------
+    //static final double DEFAULT_ZOOM       = 800.0;
+    static final double DEFAULT_REAL_N = -1.0;
+    static final double DEFAULT_REAL_P = +1.0;
+    static final double DEFAULT_IMG_N  = -1.0;
+    static final double DEFAULT_IMG_P  = +1.0;
+	
+	//double zoomFactor = DEFAULT_ZOOM;
+	double real_p   = DEFAULT_REAL_P;
+    double real_n   = DEFAULT_REAL_N;
+    double img_n   = DEFAULT_IMG_N;
+    double img_p   = DEFAULT_IMG_P;
+    //---------------------------------------------------
+    /*
+    Main Window
+    */
+    public void setGUIprop(){
+        this.setTitle("Fractals");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(WIDTH,HEIGHT);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public void addPanel(){
+        panel = new Panel();
+        fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        panel.setVisible(true);
+        this.add(panel,BorderLayout.CENTER);
+    }
+
+    // -------------------------------------------------------------------
+	private double getXPos(double x) {
+		return (x/WIDTH)*(real_p-real_n) + real_n;
+	} // getXPos
+// -------------------------------------------------------------------
+	private double getYPos(double y) {
+		return (y/HEIGHT)*(img_n-img_p) + img_p;
+	} // getYPos
+// -------------------------------------------------------------------
+
+	/**
+	 * Updates the fractal by computing the number of iterations
+	 * for each point in the fractal and changing the color
+	 * based on that.
+	 **/
+	
+	// public void updateFractal() {
 		
-		// public Canvas() {
-		// 	addMouseListener(this);
-		// } 
-		
-		@Override public Dimension getPreferredSize() {
-			return new Dimension(WIDTH, HEIGHT);
-		} // getPreferredSize
-		
-		@Override public void paintComponent(Graphics drawingObj) {
-			drawingObj.drawImage( fractalImage, 0, 0, null );
-		} // paintComponent
-		
-		// @Override public void mousePressed(MouseEvent mouse) {
-			
-		// 	double x = (double) mouse.getX();
-		// 	double y = (double) mouse.getY();
-			
-		// 	switch( mouse.getButton() ) {
+	// 	for (int x = 0; x < WIDTH; x++ ) {
+	// 		for (int y = 0; y < HEIGHT; y++ ) {
 				
-		// 		// Left
-		// 		case MouseEvent.BUTTON1:
-		// 			adjustZoom( x, y, zoomFactor*2 );
-		// 			break;
+	// 			double c_r = getXPos(x);
+	// 			double c_i = getYPos(y);
 				
-		// 		// Right
-		// 		case MouseEvent.BUTTON3:
-		// 			adjustZoom( x, y, zoomFactor/2 );
-		// 			break;
+	// 			int iterCount = computeIterations(c_r, c_i);
 				
-		// 	}
-			
-		// } // mousePressed
+	// 			int pixelColor = makeColor(iterCount);
+	// 			fractalImage.setRGB(x, y, pixelColor);
+				
+	// 		}
+	// 	}
 		
-		// public void addKeyStrokeEvents() {
-			
-		// 	KeyStroke wKey = KeyStroke.getKeyStroke(KeyEvent.VK_W, 0 );
-		// 	KeyStroke aKey = KeyStroke.getKeyStroke(KeyEvent.VK_A, 0 );
-		// 	KeyStroke sKey = KeyStroke.getKeyStroke(KeyEvent.VK_S, 0 );
-		// 	KeyStroke dKey = KeyStroke.getKeyStroke(KeyEvent.VK_D, 0 );
-			
-		// 	Action wPressed = new AbstractAction() {
-		// 		@Override public void actionPerformed(ActionEvent e) {
-		// 			moveUp();
-		// 		}
-		// 	};
-			
-		// 	Action aPressed = new AbstractAction() {
-		// 		@Override public void actionPerformed(ActionEvent e) {
-		// 			moveLeft();
-		// 		}
-		// 	};
-			
-		// 	Action sPressed = new AbstractAction() {
-		// 		@Override public void actionPerformed(ActionEvent e) {
-		// 			moveDown();
-		// 		}
-		// 	};
-			
-		// 	Action dPressed = new AbstractAction() {
-		// 		@Override public void actionPerformed(ActionEvent e) {
-		// 			moveRight();
-		// 		}
-		// 	}; 
-			
-		// 	this.getInputMap().put( wKey, "w_key" );
-		// 	this.getInputMap().put( aKey, "a_key" );
-		// 	this.getInputMap().put( sKey, "s_key" );
-		// 	this.getInputMap().put( dKey, "d_key" );
-			
-		// 	this.getActionMap().put( "w_key", wPressed );
-		// 	this.getActionMap().put( "a_key", aPressed );
-		// 	this.getActionMap().put( "s_key", sPressed );
-		// 	this.getActionMap().put( "d_key", dPressed );
-			
-		// } // addKeyStrokeEvents
+	// 	panel.repaint();
 		
-		// @Override public void mouseReleased(MouseEvent mouse){ }
-		// @Override public void mouseClicked(MouseEvent mouse) { }
-		// @Override public void mouseEntered(MouseEvent mouse) { }
-		// @Override public void mouseExited (MouseEvent mouse) { }
+    // } // updateFractal
+    static double c_r = 3;
+    static double c_i = 4;
+    public void updateFractal() {
+
+
+		for (int x = 0; x < WIDTH; x++ ) {
+			for (int y = 0; y < HEIGHT; y++ ) {
+
+                double z_r = getXPos(x);
+                double z_i = getYPos(y);
+				int iterCount = computeIterations_2(z_r, z_i);
+				
+				int pixelColor = makeColor(iterCount);
+				fractalImage.setRGB(x, y, pixelColor);
+				
+			}
+		}
 		
-	} // Canvas
+		panel.repaint();
+		
+    } // updateFractal
+    
+    // -------------------------------------------------------------------	
+	/** Returns a posterized color based off of the iteration count
+	    of a given point in the fractal **/
+	private int makeColor( int iterCount ) {
+		
+		if (iterCount == MAX_N) 
+			return Color.BLACK.getRGB();
+		
+		return Color.BLUE.getRGB();
+		
+	} // makeColor
+
+// -------------------------------------------------------------------
+    private int computeIterations_2(double z_r, double z_i) {
+            
+        /*
+        
+        Let c = c_r + c_i
+        Let z = z_r + z_i
+        
+        z' = z*z + c
+        = (z_r + z_i)(z_r + z_i) + (c_r + c_i)
+        = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
+
+            z_r' = z_r² - z_i² + c_r
+            z_i' = 2*z_i*z_r + c_i
+            
+        */
+
+        // double z_r = 0.0;
+        // double z_i = 0.0;
+        
+        int iterCount = 0;
+
+        // Modulus (distance) formula:
+        // √(a² + b²) <= 2.0
+        // a² + b² <= 4.0
+        while ( z_r*z_r + z_i*z_i <= 4.0 ) {
+            
+            double z_r_tmp = z_r;
+            
+            z_r = z_r*z_r - z_i*z_i + c_r;
+            z_i = 2*z_i*z_r_tmp + c_i;
+            // System.out.println("Inside");
+            // Point was inside the Mandelbrot set
+            if (iterCount >= MAX_N) 
+                return MAX_N;
+            
+            iterCount++;
+            
+        }
+        
+        // Complex point was outside Mandelbrot set
+        
+        // System.out.println("Outside");
+        return iterCount;
+        
+    } // computeIterations
+	// private int computeIterations(double c_r, double c_i) {
+		
+	// 	/*
+		
+	// 	Let c = c_r + c_i
+	// 	Let z = z_r + z_i
+		
+	// 	z' = z*z + c
+	// 	   = (z_r + z_i)(z_r + z_i) + (c_r + c_i)
+	// 	   = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
+
+	// 	     z_r' = z_r² - z_i² + c_r
+	// 	     z_i' = 2*z_i*z_r + c_i
+		     
+	// 	*/
+
+	// 	double z_r = 0.0;
+	// 	double z_i = 0.0;
+		
+	// 	int iterCount = 0;
+
+	// 	// Modulus (distance) formula:
+	// 	// √(a² + b²) <= 2.0
+	// 	// a² + b² <= 4.0
+	// 	while ( z_r*z_r + z_i*z_i <= 4.0 ) {
+			
+	// 		double z_r_tmp = z_r;
+			
+	// 		z_r = z_r*z_r - z_i*z_i + c_r;
+	// 		z_i = 2*z_i*z_r_tmp + c_i;
+			
+	// 		// Point was inside the Mandelbrot set
+	// 		if (iterCount >= MAX_N) 
+	// 			return MAX_N;
+			
+	// 		iterCount++;
+			
+	// 	}
+		
+	// 	// Complex point was outside Mandelbrot set
+	// 	return iterCount;
+		
+	// } // computeIterations
+// -------------------------------------------------------------------
+    public static void main(String[] args) {
+        new FractalExplorer();  //creating a new instance of Fractal
+    }
+
+
+    private class Panel extends JPanel{    //inherit JPanel
+
+        @Override
+        public Dimension getPreferredSize(){    //Dimension class encapsulates the width and height of a component (in integer precision) in a single object
+            return new Dimension(WIDTH,HEIGHT);
+        }
+
+        @Override
+        public void paintComponent(Graphics g){
+            g.drawImage(fractalImage, 0, 0, null);
+        }
+    }
 }

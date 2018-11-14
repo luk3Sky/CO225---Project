3a9e1f79634c5626d279586655b622709784954b
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-11-14 08:58:33 +0530

    Mandrelbot displaying | initial

--- Fractal.java
+++ Fractal.java
@@ -15,36 +15,33 @@ public class Fractal extends JFrame{    //inherit JFrame
     static final int WIDTH=800;
     static final int HEIGHT=800;
 
+    //may change 
     static final int MAX_N=1000;    //maximum no. of iterations
 
     //panel
     Panel panel;
     BufferedImage fractalImage;
-    
+
     /*
     constructor
     */
     public Fractal(){
         setGUIprop();
         addPanel();
+        updateFractal();
+		this.setVisible(true);
     }
 
+
+    //may change---------------------------------------
     static final double DEFAULT_ZOOM       = 100.0;
-	static final double DEFAULT_TOP_LEFT_X = -3.0;
-	static final double DEFAULT_TOP_LEFT_Y = +3.0;
+	static final double DEFAULT_TOP_LEFT_X = -2.0;
+	static final double DEFAULT_TOP_LEFT_Y = +2.0;
 	
 	double zoomFactor = DEFAULT_ZOOM;
 	double topLeftX   = DEFAULT_TOP_LEFT_X;
     double topLeftY   = DEFAULT_TOP_LEFT_Y;
-
-
-    public void addPanel(){
-        panel = new Panel();
-        fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-        this.add(panel,BorderLayout.CENTER);
-        panel.setVisible(true);
-    }
-
+    //---------------------------------------------------
     /*
     Main Window
     */
@@ -57,7 +54,107 @@ public class Fractal extends JFrame{    //inherit JFrame
         this.setVisible(true);
     }
 
+    public void addPanel(){
+        panel = new Panel();
+        fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        panel.setVisible(true);
+        this.add(panel,BorderLayout.CENTER);
+    }
 
+    // -------------------------------------------------------------------
+	private double getXPos(double x) {
+		return x/zoomFactor + topLeftX;
+	} // getXPos
+// -------------------------------------------------------------------
+	private double getYPos(double y) {
+		return y/zoomFactor - topLeftY;
+	} // getYPos
+// -------------------------------------------------------------------
+
+	/**
+	 * Updates the fractal by computing the number of iterations
+	 * for each point in the fractal and changing the color
+	 * based on that.
+	 **/
+	
+	public void updateFractal() {
+		
+		for (int x = 0; x < WIDTH; x++ ) {
+			for (int y = 0; y < HEIGHT; y++ ) {
+				
+				double c_r = getXPos(x);
+				double c_i = getYPos(y);
+				
+				int iterCount = computeIterations(c_r, c_i);
+				
+				int pixelColor = makeColor(iterCount);
+				fractalImage.setRGB(x, y, pixelColor);
+				
+			}
+		}
+		
+		panel.repaint();
+		
+    } // updateFractal
+    
+    // -------------------------------------------------------------------	
+	/** Returns a posterized color based off of the iteration count
+	    of a given point in the fractal **/
+	private int makeColor( int iterCount ) {
+		
+		if (iterCount == MAX_N) 
+			return Color.BLACK.getRGB();
+		
+		return Color.BLUE.getRGB();
+		
+	} // makeColor
+
+// -------------------------------------------------------------------
+
+	private int computeIterations(double c_r, double c_i) {
+		
+		/*
+		
+		Let c = c_r + c_i
+		Let z = z_r + z_i
+		
+		z' = z*z + c
+		   = (z_r + z_i)(z_r + z_i) + (c_r + c_i)
+		   = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
+
+		     z_r' = z_r² - z_i² + c_r
+		     z_i' = 2*z_i*z_r + c_i
+		     
+		*/
+
+		double z_r = 0.0;
+		double z_i = 0.0;
+		
+		int iterCount = 0;
+
+		// Modulus (distance) formula:
+		// √(a² + b²) <= 2.0
+		// a² + b² <= 4.0
+		while ( z_r*z_r + z_i*z_i <= 4.0 ) {
+			
+			double z_r_tmp = z_r;
+			
+			z_r = z_r*z_r - z_i*z_i + c_r;
+			z_i = 2*z_i*z_r_tmp + c_i;
+			
+			// Point was inside the Mandelbrot set
+			if (iterCount >= MAX_N) 
+				return MAX_N;
+			
+			iterCount++;
+			
+		}
+		
+		// Complex point was outside Mandelbrot set
+		return iterCount;
+		
+	} // computeIterations
+// -------------------------------------------------------------------
     public static void main(String[] args) {
         new Fractal();  //creating a new instance of Fractal
     }
